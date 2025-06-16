ded8818e474686687e9e2789d0d26959e20a6b30
Kyle Mistele <kyle@mistele.com>
2025-06-16 14:07:34 -0500

    fix: use next/script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Pricing from "@/components/Pricing";
-import TwitterConversionTracker from "@/components/TwitterConversionTracker";
 import WhatIsIt from "@/components/WhatIsIt";
 import WhyUseIt from "@/components/WhyUseIt";
+import Script from "next/script";
 
 export default function Home() {
 	return (
@@ -15,7 +15,7 @@ export default function Home() {
 			<WhyUseIt />
 			<Pricing />
 			<Footer />
-			<TwitterConversionTracker />
+			<Script src="/x-conversion-tracking.js" />
 		</div>
 	);
 }
